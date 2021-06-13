8aaada907f0824b83d2a7701ceae3f9005493cf5
sim0n-sys <85111243+sim0n-sys@users.noreply.github.com>
2021-06-13 22:25:51 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+var CC=parseInt(document.getElementById("num").value);
+var YY=parseInt(document.getElementById("year").value);
+var MM=parseInt(document.getElementById("int").value);
+var DD=parseInt(document.getElementById("day").value);
+
+var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+
+
+ 

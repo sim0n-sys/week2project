e9fc96a672598d1a402269e26f3a181060486c8f
sim0n-sys <85111243+sim0n-sys@users.noreply.github.com>
2021-06-16 13:39:10 +0300

    Add files via upload
    I have added javascript that will help the code to calculate more effectively.

---
+++ README.md
@@ -0,0 +1,46 @@
+## AKAN NAMES
+
+## ATHOR
+Simon Mungai Njoroge
+
+#### BRIEF DESCRIPTION
+This is a website that allow you to get your akan name derived form the traditional naming of the people of Ghana 
+
+# Setup/Installation Requirements
+*Clone the repository
+*Paste it on the browser
+
+## Technologies Used
+Technologies used are as follows
+* HTML5
+* CSS
+*javascript
+
+## BDD 
+Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+ where;
+
+ CC - is the century digits. For example 1989 has CC = 19
+
+ YY - is the Year digits (1989 has YY = 89)
+
+ MM -  is the Month
+
+ DD - is the Day of the month 
+
+ mod - is the modulus function ( % )
+
+
+## contact details
+*Email Simonalexno@gmail.com
+
+### License
+Copyright SIMON MUNGAI 2021
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,68 @@
+function finding() {
 
-var CC=parseInt(document.getElementById("num").value);
-var YY=parseInt(document.getElementById("year").value);
-var MM=parseInt(document.getElementById("int").value);
-var DD=parseInt(document.getElementById("day").value);
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", " Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
-var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
-var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var CC = parseInt(document.getElementById("num").value);
+    var YY = parseInt(document.getElementById("year").value);
+    var MM = parseInt(document.getElementById("int").value);
+    var DD = parseInt(document.getElementById("day").value);
+
+
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
 
 
 
- 
+
+    if (gender === "male") {
+        if (dayOfTheWeek === 0) {
+            alert("The day you were born was on a: "+ day[0]);
+            alert("Your Akan name is: " + maleNames[0]);
+        } else if (dayOfTheWeek === 1) {
+            alert("The day you were born was on a: "+ day[1]);
+            alert("Your Akan name is: " + maleNames[1]);
+        } else if (dayOfTheWeek === 2) {
+            alert( "The day you were born was on a: "+ day[2]);
+            alert("Your Akan name is: " + maleNames[2]);
+        } else if (dayOfTheWeek === 3) {
+            alert("The day you were born was on a: "+ day[3]);
+            alert("Your Akan name is: " + maleNames[3]);
+        } else if (dayOfTheWeek === 4) {
+            alert( "The day you were born was on a: "+ day[4]);
+            alert("Your Akan name is: " + maleNames[4]);
+        } else if (dayOfTheWeek === 5) {
+            alert( "The day you were born was on a: "+ day[5]);
+            alert("Your Akan name is: " + maleNames[5]);
+        } else if (dayOfTheWeek === 6) {
+            alert("The day you were born was on a: " + day[6]);
+            alert("Your Akan name is: " + maleNames[6]);
+        }
+
+    } else {
+        if (dayOfTheWeek === 0) {
+            alert("The day you were born was on a: " + day[0]);
+            alert("Your Akan name is: " + femaleNames[0]);
+        } else if (dayOfTheWeek === 1) {
+            alert( "The day you were born was on a: "+ day[1]);
+            alert("Your Akan name is: " + femaleNames[1]);
+        } else if (dayOfTheWeek === 2) {
+            alert( "The day you were born was on a: "+ day[2]);
+            alert("Your Akan name is: " + femaleNames[2]);
+        } else if (dayOfTheWeek === 3) {
+            alert( "The day you were born was on a: " + day[3]);
+            alert("Your Akan name is: " + femaleNames[3]);
+        } else if (dayOfTheWeek === 4) {
+            alert("The day you were born was on a: " + day[4]);
+            alert("Your Akan name is: " + femaleNames[4]);
+        } else if (dayOfTheWeek === 5) {
+            alert( "The day you were born was on a: "+ day[5]);
+            alert("Your Akan name is: " + femaleNames[5]);
+        } else if (dayOfTheWeek === 6) {
+            alert("The day you were born was on a: "+ day[6]);
+            alert("Your Akan name is: " + femaleNames[6]);
+        }
+    }
+
+}
